8518dd3a435cd97b96c678d264169935130e0758
D-DF <daviddefreitas0192@gmail.com>
2021-04-27 12:48:22 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import {useState} from "react"
+import CityForm from "./components/CityForm"
 import WeatherApp from "./components/WeatherApp"
 
 function App() {
+
+  const [city, setCity] = useState("Paris")
+
   return (
     <div className="container my-4">
       <h1 className="display-3 text-center mb-4">Météo Actuelle</h1>
-      <WeatherApp />
+      <WeatherApp city={city}/>
+      <CityForm setCity={setCity}/>
     </div>
   )
 }
--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -1,7 +1,52 @@
-import React from "react"
+import { useState, useEffect } from "react"
+import Icon from "./Icon"
+import Description from "./Description"
+import Temperature from "./Temperature"
+import Humidity from "./Humidity"
 
-const WeatherApp = () => {
-  return <div></div>
+const WeatherApp = ({ city }) => {
+  const [conditions, setConditions] = useState({})
+  const [description, setDescription] = useState("")
+  const [iconID, setIconID] = useState("")
+  const [location, setLocation] = useState("")
+  const {mainTemp, feelsLike, humidity} = conditions
+
+  useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}&units=metric&lang=fr`
+
+    fetch(url)
+      .then((response) => {
+        console.log(response)
+        if (!response.ok) {
+          throw new Error("Météo untrouvable")
+        }
+        return response.json()
+      })
+      .then((data) => {
+        console.log(data)
+        setConditions({
+          mainTemp: Math.round(data.main.temp),
+          feelsLike: Math.round(data.main.feelsLike),  
+          feelsLike: Math.round(data.main.humidity),
+        })
+        setDescription(data.weather[0].description)
+        setIconID(data.weather[0].icon)
+        setLocation(`${data.name}, ${data.sys.country}`)
+      })
+      .catch((error) => {
+        alert(error.message)
+      })
+  }, [city])
+
+  return (
+    <section className="text-center">
+       <Icon iconID={iconID} />
+          <h2 className="mb-4">Conditions météo à {location}</h2>
+          <Description description={description} />
+          <Temperature mainTemp={mainTemp} feelsLike={feelsLike} />
+          <Humidity humidity={humidity} />
+    </section>
+  )
 }
 
 export default WeatherApp
